bb40b63906e7428516ea317878c96fa84c1fbd12
Christian Loth <christian@loth.me>
2022-03-05 12:20:08 -0600

    Cleaning up, webapp now in diff AWS repo

---
+++ Old Web App/js/query_btn.js
@@ -0,0 +1,11 @@
+
+// this happens when the query button is pressed
+function queryBtnListener() {
+    alert("Database queried!");
+
+}
+
+// This function will pull the data from the SQL database
+function sqlPull() {
+
+}
